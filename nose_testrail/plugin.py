797833ae10e2c407822107cc05f918f5b3f117ce
Kip Gebhardt <kgebhardt23@gmail.com>
2016-11-23 12:30:19 -0800

    Adding option to output a CSV of TestClass:Testname, TESTRAIL_ID and/or report

--- nose_testrail/plugin.py
+++ nose_testrail/plugin.py
@@ -1,6 +1,7 @@
-"""Plugin to send result to the test rail"""
+"""Plugin to send test results to TestRail and/or record tests->testrail_id mappings"""
 import base64
 from datetime import datetime
+import io
 import json
 import os
 import traceback
@@ -9,27 +10,28 @@ import urllib2
 from nose.plugins import Plugin
 
 CASE_ID = 'case_id'
-# TESTRAIL_TESTCASE_ID = 'testrail_testcase_id'
 
-# def elapsed_time(seconds, separator=' '):
-#     suffixes = ['y', 'w', 'd', 'h', 'm', 's']
-#     time = []
-#     parts = [
-#         (suffixes[0], 60 * 60 * 24 * 7 * 52),
-#         (suffixes[1], 60 * 60 * 24 * 7),
-#         (suffixes[2], 60 * 60 * 24),
-#         (suffixes[3], 60 * 60),
-#         (suffixes[4], 60),
-#         (suffixes[5], 1)
-#     ]
-#     for suffix, length in parts:
-#         value = seconds / length
-#         if value > 0:
-#             seconds = seconds % length
-#             time.append('%s%s' % (str(value), suffix))
-#         if seconds < 1:
-#             break
-#     return ' '.join(time)
+
+# TODO, move inside class
+def elapsed_time(seconds, separator=' '):
+    suffixes = ['y', 'w', 'd', 'h', 'm', 's']
+    time = []
+    parts = [
+        (suffixes[0], 60 * 60 * 24 * 7 * 52),
+        (suffixes[1], 60 * 60 * 24 * 7),
+        (suffixes[2], 60 * 60 * 24),
+        (suffixes[3], 60 * 60),
+        (suffixes[4], 60),
+        (suffixes[5], 1)
+    ]
+    for suffix, length in parts:
+        value = seconds / length
+        if value > 0:
+            seconds = seconds % length
+            time.append('%s%s' % (str(value), suffix))
+        if seconds < 1:
+            break
+    return ' '.join(time)
 
 
 def case_id(id):
@@ -39,12 +41,14 @@ def case_id(id):
         return ob
     return wrap_ob
 
-# def testrail_testcase_id(id):
-#     """Decorator that adds test case id to a test"""
-#     def wrap_ob(ob):
-#         setattr(ob, TESTRAIL_TESTCASE_ID, id)
-#         return ob
-#     return wrap_ob
+
+def testrail_case_id(id):
+    return case_id(id)
+    # """Decorator that adds test case id to a test"""
+    # def wrap_ob(ob):
+    #     setattr(ob, CASE_ID, id)
+    #     return ob
+    # return wrap_ob
 
 
 class NoseTestRail(Plugin):
@@ -57,6 +61,14 @@ class NoseTestRail(Plugin):
         super(NoseTestRail, self).configure(options, conf)
         if not self.enabled:
             return
+        else:
+            self.testrail = {}
+            self.testrail.host = os.environ.get('TESTRAIL_HOST', None)
+            self.testrail.user = os.environ.get('TESTRAIL_USERNAME', None)
+            self.testrail.password = os.environ.get('TESTRAIL_PASSWORD', None)
+            self.testrail.run_id = os.environ.get('TESTRAIL_RUN_ID', None)
+            self.testrail.mapping_file = os.environ.get('TESTRAIL_MAPPING_FILE', None)
+            self.testrail.mapping_only = os.environ.get('TESTRAIL_MAPPING_ONLY', None)
 
     def begin(self):
         self.time_before = datetime.now()
@@ -66,88 +78,96 @@ class NoseTestRail(Plugin):
         self.result = {}
 
     def stopTest(self, test):
+
+        test_class = test.id().split('.')[-2]
+        test_name = test.id().split('.')[-1]
+        test_method = getattr(test.test, test_name)
+
+        test_case_id = getattr(test_method, CASE_ID)
+
+        if test_case_id:
+            if self.testrail.mapping_file:
+                with io.open(self.testrail.mapping_file, 'ab') as f:
+                    f.write('%s:%s,%d' % (test_class, test_name, test_case_id))
+
+            # Check for TestRail API Requirements before going further
+            if (
+                self.testrail.host and
+                self.testrail.user and
+                self.testrail.password and
+                self.testrail.run_id and
+                not self.testrail.mapping_only
+            ):
+                time_after = datetime.now()
+                delta = time_after - self.time_before
+                self.result['elapsed'] = elapsed_time(delta.seconds)
+                self.time_before = time_after
+                self.send_result(self.result)
+
+    def addSuccess(self, test):
+        self.result['status_id'] = 1
+        self.result['comment'] = 'test PASS'
+
+    def addFailure(self, test, err):
+        self.result['status_id'] = 5
+        self.result['comment'] = self.formatErr(err)
+
+    def addError(self, test, err):
+        self.result['status_id'] = 5
+        self.result['comment'] = self.formatErr(err)
+
+    def send_result(self, result):
+            uri = 'https://{0}/index.php?/api/v2/add_result_for_case/{1}/{2}'.format(
+                self.testrailhost,
+                self.testrail.run_id,
+                self.test_case_id
+            )
+            self.__send_request('POST', uri, result)
+
+    def __send_request(self, method, uri, data):
+        request = urllib2.Request(uri)
+        if (method == 'POST'):
+            request.add_data(json.dumps(data))
+        auth = base64.b64encode('%s:%s' % (
+            self.testrail.user,
+            self.testrail.password,
+        ))
+        request.add_header('Authorization', 'Basic %s' % auth)
+        request.add_header('Content-Type', 'application/json')
+
+        try:
+            response = urllib2.urlopen(request).read()
+        except urllib2.HTTPError as e:
+            response = e.read()
+
+        if response:
+            result = json.loads(response)
+        else:
+            result = {}
+
+        if e is not None:
+            if result and 'error' in result:
+                error = '"' + result['error'] + '"'
+            else:
+                error = 'No additional error message received'
+            raise APIError('TestRail API returned HTTP %s (%s)' % (e.code, error))
+
+        return result
+
+    def formatErr(self, err):
+        """format error"""
+        exctype, value, tb = err
+        tr = traceback.format_exception(exctype, value, tb)
+        return '' . join(tr)
+
+    def get_test_case_id(self, test):
         test_name = test.id().split('.')[-1]
         test_method = getattr(test.test, test_name)
         try:
             test_case_id = getattr(test_method, CASE_ID)
         except AttributeError:
             test_case_id = None
-        if test_case_id:
-            with open_atomic('test.txt', 'w') as f:
-                f.write('%s:%s,%d' % (test_name, test_method, test_case_id))
-
-        # time_after = datetime.now()
-        # delta = time_after - self.time_before
-        # self.result['elapsed'] = elapsed_time(delta.seconds)
-        # self.time_before = time_after
-        # self.send_result(self.result)
-
-    # def addSuccess(self, test):
-    #     self.result['status_id'] = 1
-    #     self.result['comment'] = 'test PASS'
-
-    # def addFailure(self, test, err):
-    #     self.result['status_id'] = 5
-    #     self.result['comment'] = self.formatErr(err)
-
-    # def addError(self, test, err):
-    #     self.result['status_id'] = 5
-    #     self.result['comment'] = self.formatErr(err)
-
-    # def send_result(self, result):
-    #     if self.test_case_id:
-    #         host = os.environ['TESTRAIL_HOST']
-    #         run_id = os.environ['TESTRAIL_RUN_ID']
-    #         if host:
-    #             uri = 'https://{0}/index.php?/api/v2/add_result_for_case/{1}/{2}'.format(
-    #                 host, run_id, self.test_case_id)
-    #             self.__send_request('POST', uri, result)
-
-    # def __send_request(self, method, uri, data):
-    #     request = urllib2.Request(uri)
-    #     if (method == 'POST'):
-    #         request.add_data(json.dumps(data))
-    #     user = os.environ['TESTRAIL_USERNAME']
-    #     password = os.environ['TESTRAIL_PASSWORD']
-    #     auth = base64.b64encode('%s:%s' % (user, password))
-    #     request.add_header('Authorization', 'Basic %s' % auth)
-    #     request.add_header('Content-Type', 'application/json')
-
-    #     e = None
-    #     try:
-    #         response = urllib2.urlopen(request).read()
-    #     except urllib2.HTTPError as e:
-    #         response = e.read()
-
-    #     if response:
-    #         result = json.loads(response)
-    #     else:
-    #         result = {}
-
-    #     if e is not None:
-    #         if result and 'error' in result:
-    #             error = '"' + result['error'] + '"'
-    #         else:
-    #             error = 'No additional error message received'
-    #         raise APIError('TestRail API returned HTTP %s (%s)' %
-    #             (e.code, error))
-
-    #     return result
-
-    # def formatErr(self, err):
-    #     """format error"""
-    #     exctype, value, tb = err
-    #     tr = traceback.format_exception(exctype, value, tb)
-    #     return "".join(tr)
-
-    # def get_test_case_id(self, test):
-    #     test_name = test.id().split('.')[-1]
-    #     test_method = getattr(test.test, test_name)
-    #     try:
-    #         test_case_id = getattr(test_method, CASE_ID)
-    #     except AttributeError:
-    #         test_case_id = None
-    #     return test_case_id
+        return test_case_id
 
 
 class APIError(Exception):
